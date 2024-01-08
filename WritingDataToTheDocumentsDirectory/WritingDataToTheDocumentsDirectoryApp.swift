8ae0c54c16d672d99e9d962c3cdc7b9b5d6fc87b
Ramill <ram4ik@hotmail.com>
2024-01-08 19:14:53 +0200

    Initial Commit

---
+++ WritingDataToTheDocumentsDirectory/WritingDataToTheDocumentsDirectoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WritingDataToTheDocumentsDirectoryApp.swift
+//  WritingDataToTheDocumentsDirectory
+//
+//  Created by admin on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct WritingDataToTheDocumentsDirectoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
